308d629fa3949967081852175717e170171f000c
Jagdish <2021ucp1385@mnit.ac.in>
2024-08-13 00:26:59 +0530

    User identification

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from 'express';
-import { Server } from 'socket.io';
+import {Server} from 'socket.io';
 import cors from 'cors';
 import http from 'http';
 
 const app = express();
 
-// 1.Create server using http.
+// 1. Creating server using http.
 const server = http.createServer(app);
 
 // 2. Create socket server.
-const io = new Server(server, {
+const io = new Server(server,{
     cors:{
         origin:'*',
         methods:["GET", "POST"]
@@ -17,8 +17,13 @@ const io = new Server(server, {
 });
 
 // 3. Use socket events.
+
 io.on('connection', (socket)=>{
     console.log("Connection is established");
+    socket.on('new_message', (message)=>{
+        // broadcast this message to all the clients.
+        socket.broadcast.emit('broadcast_message', message);
+    })
 
     socket.on('disconnect', ()=>{
         console.log("Connection is disconnected");
